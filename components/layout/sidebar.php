a584f57626f01071f68a8d4355fca8e8d408fe39
Sleeper678 <umardika678@gmail.com>
2024-10-29 08:15:01 +0700

    first commit

---
+++ components/layout/sidebar.php
@@ -0,0 +1,93 @@
+
+<div class="main-sidebar sidebar-style-2">
+<aside id="sidebar-wrapper">
+  <div class="sidebar-brand">
+    <a href="index.html">Padang Minang Banget</a>
+  </div>
+  <div class="sidebar-brand sidebar-brand-sm">
+    <a href="index.html">St</a>
+  </div>
+  <ul class="sidebar-menu">
+  <li>
+      <a class="nav-link" href="./../views/index.php"
+        ><i class="fas fa-fire"></i><span>Dashboard</span></a
+        ></a
+      >
+    </li>
+  
+    <li class="menu-header">Menu</li>
+    <li class="dropdown">
+      <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"
+        ><i class="fas fa-columns"></i> <span>Kategori</span></a
+      >
+      <ul class="dropdown-menu">
+    <li>
+          <a class="nav-link" href="layout-default.html"
+            >Home Kategori</a
+          >
+        </li>
+        <li>
+          <a class="nav-link" href="layout-transparent.html"
+            >Tambah Kategori</a
+          >
+        </li>
+        
+      </ul>
+    </li>
+    <li class="dropdown">
+      <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"
+        ><i class="far fa-square"></i> <span>Menu</span></a
+        ></a
+      >
+      <ul class="dropdown-menu">
+    <li>
+          <a class="nav-link" href="layout-default.html"
+            >Home Menu</a
+          >
+        </li>
+        <li>
+          <a class="nav-link" href="layout-transparent.html"
+            >Tambah Menu</a
+          >
+        </li>
+        
+      </ul>
+    </li>
+    <li class="menu-header">Pesanan</li>
+    <li class="dropdown">
+      <a href="#" class="nav-link has-dropdown"
+        ><i class="fas fa-money-bill-wave-alt"></i><span>Pesanan</span></a
+      >
+      <ul class="dropdown-menu">
+      
+        <li>
+          <a class="nav-link" href="bootstrap-table.html">Home Pesanan</a>
+        </li>
+        <li>
+          <a class="nav-link" href="bootstrap-tooltip.html"
+            >Buat Pesanan</a
+          >
+        </li>
+      </ul>
+    </li>
+    <li class="menu-header">Akun</li>
+    <li class="dropdown">
+      <a href="#" class="nav-link has-dropdown"
+        ><i class="far fa-user"></i><span>Akun</span></a
+      >
+      <ul class="dropdown-menu">
+      
+        <li>
+          <a class="nav-link" href="bootstrap-table.html">Home Pesanan</a>
+        </li>
+        <li>
+          <a class="nav-link" href="bootstrap-tooltip.html"
+            >Buat Pesanan</a
+          >
+        </li>
+    
+  </ul>
+
+ 
+</aside>
+</div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . "/DB/Connection.php";
+require_once __DIR__ . "/Model/Model.php";
+require_once __DIR__ . "/Model/Cars.php";
+
+$data_mobil = ['name' => 'Ayla', 'brand' => "Yamaha",'color' => "Merah" ];
+$ayla = new Cars();
+
+echo "<br>";
+var_dump($ayla->all());
+echo "<br>";
+// var_dump($ayla->create($data_mobil));
+echo "<br>";
+var_dump($ayla->find(11));
+
+header("Location: ./../pos/views/index.php");
+
